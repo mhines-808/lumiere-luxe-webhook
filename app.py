805164378f204e38901c4be0cad996161d6afa5b
Mark Hines <ewebtrends@gmail.com>
2026-09-17 16:27:50 -0700

    Add SSL mode for Neon connection

--- app.py
+++ app.py
@@ -19,7 +19,8 @@ DB_CONFIG = {
     "user": os.environ.get("DB_USER"),
     "password": os.environ.get("DB_PASSWORD"),
     "host": os.environ.get("DB_HOST"),
-    "port": os.environ.get("DB_PORT")
+    "port": os.environ.get("DB_PORT", "5432"),
+    "sslmode": "require"
 }
 
 
